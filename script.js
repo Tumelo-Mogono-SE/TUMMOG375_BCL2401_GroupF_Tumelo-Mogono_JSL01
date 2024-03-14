2ddda5ccb60c750007abb1fb23fddb896f3ff050
Tumelo Mogono <tumelomogono@gmail.com>
2024-03-14 14:39:12 +0200

    Added validation Logic

--- script.js
+++ script.js
@@ -5,8 +5,15 @@ function validateSyntax() {
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
+    let validator = /^pet_(19|20)\d{2}[A-Z]\w+$/;
 
-            document.getElementById('result').innerText = result;
+    if (validator.test(input)){
+        result  = "Valid Syntax";
+    } else {
+        result = "Invalid Syntax";
+    }
+
+    document.getElementById('result').innerText = result;
 }
 
 
